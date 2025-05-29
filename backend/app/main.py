236cbc57d1c63bc6fa926d1f976bf135296a147b
rrraumpanzer <ayylmao3713@gmail.com>
2025-05-29 11:00:01 +0300

    last additions

--- backend/app/main.py
+++ backend/app/main.py
@@ -63,7 +63,7 @@ async def log_request_response(request: Request, call_next):
         media_type=response.media_type
     )
 
-app.middleware("http")(log_request_response)
+#app.middleware("http")(log_request_response)
 
 AVATARS_DIR = "static/avatars"
 IMAGES_DIR = "static/images"
@@ -74,10 +74,6 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.include_router(user_router, prefix="/user")
 app.include_router(recipe_router, prefix="/recipe")
-
-@app.get('/test')
-async def reply():
-    return JSONResponse(status_code=200, content={"message":"Test tested testessfully!"})
         
 if __name__ == "app":
     import uvicorn
