3bd7c13d701e2fc87c3287572ffce1b5e535ecd4
rrraumpanzer <162184962+rrraumpanzer@users.noreply.github.com>
2025-05-31 13:44:54 +0300

    connection.py

--- backend/app/database/connection.py
+++ backend/app/database/connection.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL: str = 'postgresql://web_rsp_service:Qici45d@localhost:5432/recipespace_database'
+DATABASE_URL: str = 'postgresql://web_rsp_service:*пароль*@localhost:5432/recipespace_database'
 # postgresql://user:password@host:port/database
 
 engine = create_engine(DATABASE_URL)
